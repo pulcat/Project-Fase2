7b7438aa77fc95cde9a7e262c6a9177706ed7882
Diego perdomo <diegosebastian.com@gmail.com>
2025-03-15 23:55:18 -0400

    Added query (C)

---
+++ Querys/c - Productos Recomendados en Carritos Ajenos.sql
@@ -0,0 +1,8 @@
+SELECT Producto.nombre "Nombre", Categoria.nombre "Categoria", Marca.nombre "Marca"
+FROM Producto
+join Categoria on Categoria.id = Producto.categoriaId
+join Marca on Marca.id = Producto.marcaId
+join ProductoRecomendadoParaCliente on ProductoRecomendadoParaCliente.productoRecomendadoId = Producto.id
+join Factura on Factura.clienteId = ProductoRecomendadoParaCliente.clienteId
+join Carrito on Carrito.productoId = ProductoRecomendadoParaCliente.productoRecomendadoId
+WHERE DATEADD(day, 30, fechaEmision) >= GETDATE() AND ProductoRecomendadoParaCliente.clienteId != Carrito.clienteId
